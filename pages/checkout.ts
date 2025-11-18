3f55da01f36c34676b137883175f2e870c71630d
Shahnewaz Jolly <shahnewaz.amin@gmail.com>
2025-11-18 13:07:47 -0700

    feature-branch - Add tests for checkout continue function

--- pages/checkout.ts
+++ pages/checkout.ts
@@ -1,4 +1,5 @@
 import { Page, Locator } from "@playwright/test";
+import { getRandomString } from "../helpers/stringhelper";
 
 export class CheckoutPage {
   readonly page: Page;
@@ -9,6 +10,7 @@ export class CheckoutPage {
   readonly postalCodeInputField: Locator;
   readonly cancelButton: Locator;
   readonly continueButton: Locator;
+  readonly checkoutConfirmationUrl: string;
 
   constructor(page: Page) {
     this.page = page;
@@ -18,5 +20,14 @@ export class CheckoutPage {
     this.postalCodeInputField = page.locator("[data-test='postalCode']");
     this.cancelButton = page.locator("[data-test='cancel']");
     this.continueButton = page.locator('[data-test="continue"]');
+    this.checkoutConfirmationUrl = "/checkout-step-two.html";
+  }
+
+  async continueCheckout(): Promise<void> {
+    const randonString = getRandomString(10);
+    await this.firstNameInputField.fill(randonString);
+    await this.lastNameInputField.fill(randonString);
+    await this.postalCodeInputField.fill(randonString);
+    await this.continueButton.click();
   }
 }
